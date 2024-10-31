37e730ff8b0651d670f15ad08706942e6a6e4b6b
Lup Yuen Lee <luppy@appkaki.com>
2024-10-31 16:29:11 +0800

    Adding GitHub Actions

--- run.sh
+++ run.sh
@@ -7,6 +7,17 @@ set -x  #  Echo commands
 for (( ; ; )); do
   find $HOME/riscv/nuttx -name defconfig >/tmp/defconfig.txt
 
+  cargo run -- \
+    --user NuttX \
+    --repo nuttx \
+    --defconfig /tmp/defconfig.txt \
+    --file /tmp/ingest-nuttx-builds/ci-arm-01.log \
+    --group arm-01 \
+    --run-id 11603561928 \
+    --job-id 32310817851 \
+    --step 7
+  exit
+
   cargo run -- --user nuttxpr  --defconfig /tmp/defconfig.txt
   date ; sleep 300
 
