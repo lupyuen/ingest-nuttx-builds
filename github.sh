ff719947f9c23f9eefa3b7c7cf91e2bdd25a0db3
Lup Yuen Lee <luppy@appkaki.com>
2024-11-01 23:01:46 +0800

    Wait until the Latest Run ID for today has completed

--- github.sh
+++ github.sh
@@ -64,64 +64,87 @@ function ingest_log {
     --step $step
 }
 
-## Generate the list of deconfigs
-defconfig=/tmp/defconfig-github.txt
-find $HOME/riscv/nuttx -name defconfig >$defconfig
+## Loop forever until the Latest Run ID for today has completed
+for (( ; ; )); do
 
-## Get the Latest Completed Run ID for today
-date=$(date -u +'%Y-%m-%d')
-run_id=$(
-  gh run list \
-    --repo $user/$repo \
-    --limit 1 \
-    --created $date \
-    --status completed \
-    --json databaseId,name,displayTitle,conclusion \
-    --jq '.[].databaseId'
-)
-echo run_id=$run_id
-if [[ "$run_id" == "" ]]; then
-  echo Quitting, no completed runs for today
-  sleep 10
-  exit
-fi
+  ## Generate the list of deconfigs
+  defconfig=/tmp/defconfig-github.txt
+  find $HOME/riscv/nuttx -name defconfig >$defconfig
 
-## For Testing
-## run_id=11603561928
-## job_id=32310817851
+  ## Get the Latest Completed Run ID for today
+  date=$(date -u +'%Y-%m-%d')
+  run_id=$(
+    gh run list \
+      --repo $user/$repo \
+      --limit 1 \
+      --created $date \
+      --status completed \
+      --json databaseId,name,displayTitle,conclusion \
+      --jq '.[].databaseId'
+  )
+  echo run_id=$run_id
+  if [[ "$run_id" == "" ]]; then
+    echo No completed runs for today, waiting...
+    sleep 300
+    continue
+  fi
 
-## Download the Run Logs
-## https://docs.github.com/en/rest/actions/workflow-runs?apiVersion=2022-11-28#download-workflow-run-logs
-curl -L \
-  --output /tmp/run-log.zip \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$user/$repo/actions/runs/$run_id/logs
+  ## Get the Latest Run ID for today. Check that it has completed.
+  latest_run_id=$(
+    gh run list \
+      --repo $user/$repo \
+      --limit 1 \
+      --created $date \
+      --json databaseId,name,displayTitle,conclusion \
+      --jq '.[].databaseId'
+  )
+  echo latest_run_id=$latest_run_id
+  if [[ "$run_id" != "$latest_run_id" ]]; then
+    echo Latest run has not completed, waiting...
+    sleep 300
+    continue
+  fi
 
-## Unzip the Log Files
-tmp_path=/tmp/ingest-nuttx-builds
-rm -rf $tmp_path
-mkdir $tmp_path
-pushd $tmp_path
-unzip /tmp/run-log.zip
-popd
+  ## For Testing
+  ## run_id=11603561928
+  ## job_id=32310817851
 
-## For All Target Groups
-## TODO: Handle macOS, msvc, msys2 when the warnings have been cleaned up
-for group in \
-  arm-01 arm-02 arm-03 arm-04 \
-  arm-05 arm-06 arm-07 arm-08 \
-  arm-09 arm-10 arm-11 arm-12 \
-  arm-13 arm-14 \
-  arm64-01 \
-  other \
-  risc-v-01 risc-v-02 risc-v-03 risc-v-04 \
-  risc-v-05 risc-v-06 \
-  sim-01 sim-02 sim-03 \
-  x86_64-01 \
-  xtensa-01 xtensa-02
-do
-  ## Ingest the Log File
-  ingest_log $group
+  ## Download the Run Logs
+  ## https://docs.github.com/en/rest/actions/workflow-runs?apiVersion=2022-11-28#download-workflow-run-logs
+  curl -L \
+    --output /tmp/run-log.zip \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/$user/$repo/actions/runs/$run_id/logs
+
+  ## Unzip the Log Files
+  tmp_path=/tmp/ingest-nuttx-builds
+  rm -rf $tmp_path
+  mkdir $tmp_path
+  pushd $tmp_path
+  unzip /tmp/run-log.zip
+  popd
+
+  ## For All Target Groups
+  ## TODO: Handle macOS, msvc, msys2 when the warnings have been cleaned up
+  for group in \
+    arm-01 arm-02 arm-03 arm-04 \
+    arm-05 arm-06 arm-07 arm-08 \
+    arm-09 arm-10 arm-11 arm-12 \
+    arm-13 arm-14 \
+    arm64-01 \
+    other \
+    risc-v-01 risc-v-02 risc-v-03 risc-v-04 \
+    risc-v-05 risc-v-06 \
+    sim-01 sim-02 sim-03 \
+    x86_64-01 \
+    xtensa-01 xtensa-02
+  do
+    ## Ingest the Log File
+    ingest_log $group
+  done
+
+  ## Run once only
+  break
 done
