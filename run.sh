4cbaf145a5799cf118df78724a00723d833df71b
Lup Yuen Lee <luppy@appkaki.com>
2024-11-09 11:34:05 +0800

    Add lvanasse to Build Farm yay!

--- run.sh
+++ run.sh
@@ -11,6 +11,13 @@ for (( ; ; )); do
   ## Find all defconfig files
   find $HOME/riscv/nuttx -name defconfig >/tmp/defconfig.txt
 
+  ## Ingest logs from lvanasse GitHub Gist. Remove special characters.
+  cargo run -- \
+    --user lvanasse \
+    --defconfig /tmp/defconfig.txt \
+    | tr -d '\\033'
+  date ; sleep 300
+
   ## Ingest logs from nuttxpr GitHub Gist. Remove special characters.
   cargo run -- \
     --user nuttxpr \
