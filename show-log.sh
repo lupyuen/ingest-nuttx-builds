ae2d0a08040169eed528b3ed1d8bb1675c512914
Lup Yuen Lee <luppy@appkaki.com>
2024-11-12 08:52:41 +0800

    Restart the log display every 12 hours, due to Log Rotation

--- show-log.sh
+++ show-log.sh
@@ -3,11 +3,18 @@
 
 set -x  #  Echo commands
 
-clear && tail -f /opt/homebrew/var/log/grafana/grafana.log \
-  | grep --line-buffered "logger=context " \
-  | grep --line-buffered -v "path=/api/frontend-metrics " \
-  | grep --line-buffered -v "path=/api/live/ws " \
-  | grep --line-buffered -v "path=/api/plugins/grafana-lokiexplore-app/settings " \
-  | grep --line-buffered -v "path=/api/user/auth-tokens/rotate " \
-  | grep --line-buffered -v "path=/favicon.ico " \
-  | cut -d ' ' -f 9-15
+for (( ; ; )); do
+  clear && tail -f /opt/homebrew/var/log/grafana/grafana.log \
+    | grep --line-buffered "logger=context " \
+    | grep --line-buffered -v "path=/api/frontend-metrics " \
+    | grep --line-buffered -v "path=/api/live/ws " \
+    | grep --line-buffered -v "path=/api/plugins/grafana-lokiexplore-app/settings " \
+    | grep --line-buffered -v "path=/api/user/auth-tokens/rotate " \
+    | grep --line-buffered -v "path=/favicon.ico " \
+    | cut -d ' ' -f 9-15 \
+    &
+
+  ## Restart the log display every 12 hours, due to Log Rotation
+  sleep $(( 12 * 60 * 60 ))
+  kill %1
+done
