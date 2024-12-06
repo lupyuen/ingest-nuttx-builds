10698192822cbe7e24cb6a31911fbac42eb8d4d0
Lup Yuen Lee <luppy@appkaki.com>
2024-12-06 21:59:54 +0800

    Add nuttxmacos2

--- run.sh
+++ run.sh
@@ -11,6 +11,17 @@ for (( ; ; )); do
   ## Find all defconfig files
   find $HOME/riscv/nuttx -name defconfig >/tmp/defconfig.txt
 
+  ## Ingest logs from nuttxmacos2/nuttx-build-log GitLab Snippets. Remove special characters.
+  ## gitlab-token.sh contains "export GITHUB_TOKEN=...", any GitLab Token with read access will do.
+  set +x ; . $HOME/gitlab-token.sh ; set -x
+  cargo run -- \
+    --user nuttxmacos2 \
+    --repo nuttx-build-log \
+    --defconfig /tmp/defconfig.txt \
+    | tr -d '\033\007'
+  unset GITLAB_TOKEN
+  date ; sleep 300
+
   ## Ingest logs from lupyuen/nuttx-build-log GitLab Snippets. Remove special characters.
   ## gitlab-token.sh contains "export GITHUB_TOKEN=...", any GitLab Token with read access will do.
   set +x ; . $HOME/gitlab-token.sh ; set -x
